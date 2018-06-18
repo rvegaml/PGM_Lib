ad6cc27629c3ea883c0d3a1ed2e42111493ee762
Roberto <rvega@ualberta.ca>
2018-06-18 13:05:13 -0600

    Adding MRF class and method for computing the likelihood of an instance given the factors

--- PGM_functions.py
+++ PGM_functions.py
@@ -1,6 +1,8 @@
 from __future__ import division
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from models import f_c_softmax_Group_LASSO
+import tensorflow as tf
 
 class Factor():
 	def __init__(self, variables, cardinality, values=[]):
@@ -32,6 +34,33 @@ class Factor():
 		# Create the one-hot encoding object
 		self.onehot_encoder = OneHotEncoder(n_values=np.int16(num_entries), sparse=False)
 		self.onehot_encoder.fit(np.reshape(range(num_entries), (-1,1)))
+
+		# Create the array that converts assignment to index
+		temp = np.hstack([self.cardinality, 1])
+		temp = np.flip(temp, axis=0)
+		temp = np.flip(np.cumprod(temp), axis=0)
+		
+		self.convert_a_to_i = temp
+
+		# Create a dictionary that maps every variable to an index
+		var_to_ind = dict()
+		for i in range(len(variables)):
+			var_to_ind[variables[i]] = i
+
+		self.var_to_ind = var_to_ind
+
+	def index_to_assignment(self, index):
+		# Extract the vector that contains the cumulative product of the cardinality
+		temp = self.convert_a_to_i[1:]
+		# Transform the vectors into matrices (This is needed to process several indexes
+		# at the same time.)
+		temp = np.matmul(np.ones([len(index), 1]), np.reshape(temp,[1,-1]))
+		temp_index = np.matmul(np.reshape(index,[-1,1]), np.ones([1, temp.shape[1]]))
+		temp_cardinality = np.matmul(np.ones([len(index), 1]), np.reshape(self.cardinality,[1,-1]))
+		# Convert the index into the actual assignment
+		temp = np.mod(np.floor(np.divide(temp_index,temp)), temp_cardinality)
+		
+		return np.int8(temp)
 	
 	def assignment_to_index(self, assignment):
 		# Function that returns the index (in the values vector) of the given assignment.
@@ -40,7 +69,7 @@ class Factor():
 		index = np.sum(temp_card*assignment, axis=1)
 		
 		return np.reshape(index, (-1,1))
-	
+
 	def full_assignment_to_index(self, x):
 		# Function that returns the index (in the values vector) of the given assignment.
 		# x is an array that contains the entire instance
@@ -71,6 +100,171 @@ class Factor():
 		
 		return self.get_value_assignment(assignment)
 
+	def print_CPT(factor):
+		'''
+		Function that prints all the assignments ni the factor along with their probabilities
+		'''
+		assignments = factor.index_to_assignment(range(np.prod(factor.cardinality)))
+		num_assignments, num_var = assignments.shape
+		CPT = np.zeros((num_assignments, num_var+1))
+		CPT[:, 0:num_var] = assignments
+		CPT[:, -1] = factor.values
+		
+		print(CPT)
+
+class MRF():
+	def __init__(self, factor_list=[], adj_matrix=[]):
+		self.factor_list = factor_list
+		self.adj_matrix = adj_matrix
+
+		if len(factor_list) == 0:
+			self.Z = 0
+		else:
+			self.Z = compute_partition_function(factor_list)
+
+	def compute_partition_function(self, factor_list):
+		# Compute the partition function for the entire distribution
+		jointDistribution = Factor(factor_list[0].variables, factor_list[0].cardinality, factor_list[0].values)
+
+		for i in range(len(factor_list)-1):
+			jointDistribution = FactorProduct(jointDistribution, factor_list[i+1])
+
+		Z = np.sum(jointDistribution.values)
+
+		return Z
+
+	def train(self, dataset, alpha=.01):
+		# Function to learn the potentials of a pairwise graph given a dataset and a regularization parameter alpha
+		num_variables = dataset.shape[1]
+		pairwise_factors = []
+		node_factors = []
+		factor_list = []
+
+		for x_id in range(num_variables):
+			tf.reset_default_graph()
+			new_dataset, Y, cardinality = transform_dataset(x_id, dataset)
+
+			num_iter = 10000
+			batch_size = 5000
+			optimizer_name = tf.train.AdamOptimizer
+
+			name = 'Test' 
+			dim_classifier = np.array([new_dataset.shape[1], cardinality[x_id]])
+			save_dir='./trained_variables_fc_H1_1000'
+
+			F_C_Net = f_c_softmax_Group_LASSO(name, dim_classifier, cardinality, alpha=.01, save_dir=save_dir)
+			F_C_Net.train(optimizer_name, new_dataset, Y, num_iter=num_iter, batch_size=batch_size)
+
+			pairwise_factors.append(F_C_Net.final_weights)
+			node_factors.append(F_C_Net.final_bias)
+
+		# Create an empty adjacency matrix
+		self.adj_matrix = np.zeros((num_variables, num_variables), dtype=np.int8)
+
+		# Create the node and pairwise factor
+		for x_id in range(num_variables):
+
+			# Node factor
+			new_Factor = Factor(np.array([x_id]), np.array([cardinality[x_id]]), np.exp(node_factors[x_id]))
+			factor_list.append(new_Factor)
+
+			rest_variables = []
+			start_index = 0
+
+			for i in range(num_variables):
+				if i != x_id:
+					rest_variables.append(i)
+
+			for y_id in rest_variables:
+				end_index = start_index + cardinality[y_id]
+
+				if y_id > x_id:
+					c_weights = pairwise_factors[x_id]
+					c_factor_values_mat = c_weights[start_index:end_index, :]
+					c_factor_values = np.reshape(c_factor_values_mat, (-1), order='F')
+
+					# Pairwise factor
+					if np.linalg.norm(c_factor_values) > 1E-6:
+						self.adj_matrix[x_id, y_id] = 1
+						self.adj_matrix[y_id, x_id] = 1
+						new_Factor = Factor(np.array([x_id, y_id]), np.array([cardinality[x_id], cardinality[y_id]]), np.exp(c_factor_values))
+						factor_list.append(new_Factor)
+				start_index = end_index
+
+		self.factor_list = factor_list
+		self.Z = self.compute_partition_function(factor_list)
+
+	def compute_likelihood(self, dataset):
+		'''
+		Function that computes the likelihood of every instance in the dataset given a trained graph
+		'''
+		num_variables = dataset.shape[1]
+		var_id = np.arange(num_variables)
+		instance_factor_list = self.factor_list
+		likelihood = np.zeros(dataset.shape[0])
+
+
+		for instance_id in range(dataset.shape[0]):
+			instance = dataset[instance_id, :]
+			instance_factor_list = self.factor_list
+
+			# Identify the variables whose values are missing
+			missing_flag = (instance == -1)
+			if np.any(missing_flag):
+				var_to_marginalize = var_id[missing_flag]
+			else:
+				var_to_marginalize = []
+
+			# Identify the factors that contain the variables to marginalize
+			factors_to_marginalze = []
+			other_factors = []
+
+			if len(var_to_marginalize) != 0:
+				for factor in self.factor_list:
+					if np.any(np.in1d(var_to_marginalize, factor.variables)):
+						factors_to_marginalze.append(factor)
+					else:
+						other_factors.append(factor)
+
+				# Marginalize all the missing variables
+				remaining_factors = []
+
+				for var in var_to_marginalize:
+					factors_with_c_variable = []
+
+					for factor in factors_to_marginalze:
+						if var in factor.variables:
+							factors_with_c_variable.append(factor)
+						else:
+							remaining_factors.append(factor)
+
+					# Multiply all the factor with the current variable
+					new_factor = factors_with_c_variable[0]
+
+					for i in range(len(factors_with_c_variable) - 1):
+						new_factor = FactorProduct(new_factor, factors_with_c_variable[i+1])
+
+					# Marginalize the current variable
+					new_factor = FactorMarginalization(new_factor, var, np.sum)
+					remaining_factors.append(new_factor)
+
+					factors_to_marginalze = remaining_factors
+					remaining_factors = []
+
+				# The new factor list are the factors after marginalization
+				instance_factor_list = factors_to_marginalze + other_factors
+
+
+			unnorm_likelihood = 0
+			for factor in instance_factor_list:
+				c_val = factor.get_value_full_assignment(np.array([instance]))[0][0]
+				unnorm_likelihood += np.log(c_val)
+
+			likelihood[instance_id] = np.exp(unnorm_likelihood) / self.Z
+
+		return likelihood
+
+
 def transform_dataset(x_id, dataset):
 	# Start by getting the cardinality of every variable on the dataset
 	num_variables = dataset.shape[1]
@@ -78,11 +272,13 @@ def transform_dataset(x_id, dataset):
 	features_var = []
 
 	for var in range(num_variables):
-		unique_values = np.unique(dataset[:, x_id])
+		unique_values = np.unique(dataset[:, var])
 		cardinality[var] = len(unique_values)
 
 		if var != x_id:	
 			features_var.append(var)
+
+	print(cardinality)
 	
 	# Determine the number of features in the vector
 	features_card = cardinality[features_var]
@@ -204,3 +400,104 @@ def initializeFactors(dataset):
 				factor_list.append(new_Factor)
 	
 	return factor_list
+
+def FactorProduct(factor_A, factor_B):
+	# This function performs the factor product operation. The resulting factor has the entries in ascending order
+	# of the variables id.
+
+	if len(factor_A.variables) == 0:
+		if len(factor_A.values) == 0:
+			factor_C = Factor(factor_B.variables, factor_B.cardinality, factor_B.values)
+		else:
+			factor_C = Factor(factor_B.variables, factor_B.cardinality, factor_B.values*factor_A.values)
+
+	if len(factor_B.variables) == 0:
+		if len(factor_B.values) == 0:
+			factor_C = Factor(factor_A.variables, factor_A.cardinality, factor_A.values)
+		else:
+			factor_C = Factor(factor_A.variables, factor_A.cardinality, factor_B.values*factor_A.values)
+
+	# Set the variables present on the new factor
+	var_C = np.union1d(factor_A.variables, factor_B.variables)
+
+	# Identify the indexes of A and B in C
+	map_A = np.zeros(len(factor_A.variables), dtype=np.int16)
+	counter = 0
+	for var in factor_A.variables:
+		map_A[counter] = np.where(np.equal(var_C, var))[0][0]
+		counter +=1
+
+	map_B = np.zeros(len(factor_B.variables), dtype=np.int16)
+	counter = 0
+	for var in factor_B.variables:
+		map_B[counter] = np.where(np.equal(var_C, var))[0][0]
+		counter += 1
+
+	# Set the cardinality of factor C
+	card_C = np.zeros(len(var_C), dtype=np.int16)
+	card_C[map_A] = factor_A.cardinality
+	card_C[map_B] = factor_B.cardinality
+
+	# Create the new factor C
+	factor_C = Factor(var_C, card_C)
+
+	# Fill the CPT
+	assignments = factor_C.index_to_assignment(range(np.prod(factor_C.cardinality)))
+	index_A = factor_A.assignment_to_index(assignments[:, map_A])
+	index_B = factor_B.assignment_to_index(assignments[:, map_B])
+
+	# To avoid underflow problems, make the multiplication in the log space
+	new_prob = np.add(np.log(factor_A.values[index_A]), np.log(factor_B.values[index_B]))
+	new_prob = np.exp(new_prob)
+
+	factor_C.values = np.reshape(new_prob, (-1))
+
+	return factor_C
+
+def FactorMarginalization(factor, var_id, operation=np.sum):
+	''' It marginalizes the var_id from the CPT using the operation defined in 'operation'.
+	The function returns the unnormalized new factor without the variable to be marginalized.
+	'''
+
+	# Find the index of the variable to marginalize
+	c_index = factor.var_to_ind[var_id]
+
+	# Find the number of possible values that this variable might take
+	c_card = factor.cardinality[c_index]
+
+	# Create a new factor without the variable to marginalize
+	new_variables = np.hstack([factor.variables[0:c_index], factor.variables[c_index+1:]])
+	new_card = np.hstack([factor.cardinality[0:c_index], factor.cardinality[c_index+1:]])
+	new_card = np.int16(new_card)
+	new_factor = Factor(new_variables, new_card)
+
+	# Find all the possible assignments of the new factor
+	num_new_assignments = np.prod(new_card)
+	new_possible_assignments = new_factor.index_to_assignment(range(num_new_assignments))
+
+	# Find all the possible assignments without the variable to marginalize
+	num_assignments = np.prod(factor.cardinality)
+	possible_assignments = factor.index_to_assignment(range(num_assignments))
+	possible_assignments = np.hstack([possible_assignments[:,0:c_index], possible_assignments[:,c_index+1:]])
+
+	# Fill the new CPT
+	for assignment in new_possible_assignments:
+		i = new_factor.assignment_to_index(np.array([assignment]))
+		prob_array = np.zeros(c_card)
+		counter = 0
+		for j in range(possible_assignments.shape[0]):
+			line = possible_assignments[j]
+			if np.array_equal(assignment, line):
+				prob_array[counter] = factor.values[j]
+				counter = counter + 1
+
+		val = operation(prob_array)
+		new_factor.values[i] = val
+
+	return new_factor
+
+def main():
+	return -1
+
+if __name__ == '__main__':
+	main()
--- Sandbox.py
+++ Sandbox.py
@@ -1,138 +1,187 @@
 from __future__ import division
 import numpy as np
-from PGM_functions import Factor, Gibbs_sampling, transform_dataset
+from PGM_functions import Factor, Gibbs_sampling, transform_dataset, FactorProduct, MRF
 from models import f_c_softmax_Group_LASSO
 import tensorflow as tf
 
 # Create a fake dataset
-# var_A = np.array([0])
-# card_A = np.array([3])
-# values_A = np.log(np.array([100, 50, 1]))
-
-# var_B = np.array([1])
-# card_B = np.array([3])
-# values_B = np.log(np.array([50, 50, 50]))
-
-# var_C = np.array([2])
-# card_C = np.array([3])
-# values_C = np.log(np.array([20,1,100]))
-
-# var_AB = np.array([0, 1])
-# card_AB = np.array([3, 3])
-# values_AB = np.log(np.array([100,20,20,20,100,20,20,20,100]))
-
-# var_AC = np.array([0, 2])
-# card_AC = np.array([3, 3])
-# values_AC = np.log(np.array([10,50,50,50,10,50,50,50,100]))
-
 var_A = np.array([0])
-card_A = np.array([3])
-values_A = np.log(np.array([10, 10, 20]))
+card_A = np.array([2])
+values_A = np.log(np.array([100, 50]))
 
 var_B = np.array([1])
-card_B = np.array([3])
-values_B = np.log(np.array([5, 5, 5]))
+card_B = np.array([2])
+values_B = np.log(np.array([50, 50]))
 
 var_C = np.array([2])
 card_C = np.array([3])
-values_C = np.log(np.array([100,1,1]))
+values_C = np.log(np.array([120,100,100]))
 
-var_D = np.array([3])
-card_D = np.array([3])
-values_D = np.log(np.array([50,10,30]))
+var_AB = np.array([0, 1])
+card_AB = np.array([2, 2])
+values_AB = np.log(np.array([100,120,120,120]))
 
 var_AC = np.array([0, 2])
-card_AC = np.array([3, 3])
-values_AC = np.log(np.array([100,10,10,10,100,10,1,1,100]))
-
-var_AD = np.array([0, 3])
-card_AD = np.array([3, 3])
-values_AD = np.log(np.array([10,50,50,50,10,50,50,50,10]))
-
-var_BD = np.array([1, 3])
-card_BD = np.array([3, 3])
-values_BD = np.log(np.array([30,1,30,30,1,30,30,1,100]))
+card_AC = np.array([2, 3])
+values_AC = np.log(np.array([100,50,50,50,100,50]))
 
 factor_A = Factor(var_A, card_A, values_A)
 factor_B = Factor(var_B, card_B, values_B)
 factor_C = Factor(var_C, card_C, values_C)
-factor_D = Factor(var_D, card_D, values_D)
+factor_AB = Factor(var_AB, card_AB, values_AB)
 factor_AC = Factor(var_AC, card_AC, values_AC)
-factor_AD = Factor(var_AD, card_AD, values_AD)
-factor_BD = Factor(var_BD, card_BD, values_BD)
 
-factor_list = [factor_A, factor_B, factor_C, factor_D, factor_AC, factor_AD, factor_BD]
+factor_list = [factor_A, factor_B, factor_C, factor_AB, factor_AC]
 
 print('Creating dataset...')
-dataset = Gibbs_sampling(factor_list, np.array([1,1,2,1]), 20000)
+dataset = Gibbs_sampling(factor_list, np.array([1,1,2]), 20000)
+
+print(np.unique(dataset[:,0]))
+print(np.unique(dataset[:,1]))
+print(np.unique(dataset[:,2]))
+print(np.min(dataset, axis=0))
 print('Dataset created \n')
-print(dataset[4990:4995,:])
+
+graph = MRF()
+graph.train(dataset)
+print(graph.adj_matrix)
+print(graph.Z)
+print(graph.compute_likelihood(np.array([[0,-1,0], [0,0,2]])))
+
+factor_list = graph.factor_list
+final_factor = factor_list[0]
+for i in range(len(factor_list)-1):
+	final_factor = FactorProduct(final_factor, factor_list[i+1])
+
+print(final_factor.values / np.sum(final_factor.values))
+prob =final_factor.values / np.sum(final_factor.values) 
+print(prob[0] + prob[3])
 
 # ----------------------------------------------------------------
-num_variables = dataset.shape[1]
-pairwise_factors = []
-node_factors = []
-factor_list = []
 
-for x_id in range(num_variables):
-	tf.reset_default_graph()
-	new_dataset, Y, cardinality = transform_dataset(x_id, dataset)
-	print(new_dataset[4990:4995,:])
-	print(Y[4990:4995,:])
-	print(cardinality)
+# var_A = np.array([0])
+# card_A = np.array([3])
+# values_A = np.log(np.array([10, 10, 20]))
 
-	num_iter = 10000
-	batch_size = 5000
-	optimizer_name = tf.train.AdamOptimizer
+# var_B = np.array([1])
+# card_B = np.array([3])
+# values_B = np.log(np.array([5, 5, 5]))
 
-	name = 'Test' 
-	dim_classifier = np.array([new_dataset.shape[1], cardinality[x_id]])
-	save_dir='./trained_variables_fc_H1_1000'
+# var_C = np.array([2])
+# card_C = np.array([3])
+# values_C = np.log(np.array([100,1,1]))
 
-	F_C_Net = f_c_softmax_Group_LASSO(name, dim_classifier, cardinality, alpha=0.01, save_dir=save_dir)
-	F_C_Net.train(optimizer_name, new_dataset, Y, num_iter=num_iter, batch_size=batch_size)
+# var_D = np.array([3])
+# card_D = np.array([3])
+# values_D = np.log(np.array([50,10,30]))
 
-	pairwise_factors.append(F_C_Net.final_weights)
-	node_factors.append(F_C_Net.final_bias)
+# var_AC = np.array([0, 2])
+# card_AC = np.array([3, 3])
+# values_AC = np.log(np.array([100,10,10,10,100,10,1,1,100]))
 
-# Create an empty adjacency matrix
-adj_matrix = np.zeros((num_variables, num_variables), dtype=np.int8)
+# var_AD = np.array([0, 3])
+# card_AD = np.array([3, 3])
+# values_AD = np.log(np.array([10,50,50,50,10,50,50,50,10]))
 
-# Create the node and pairwise factor
-for x_id in range(num_variables):
+# var_BD = np.array([1, 3])
+# card_BD = np.array([3, 3])
+# values_BD = np.log(np.array([30,1,30,30,1,30,30,1,100]))
 
-	# Node factor
-	new_Factor = Factor(np.array([x_id]), np.array([cardinality[x_id]]), node_factors[x_id])
-	factor_list.append(new_Factor)
+# factor_A = Factor(var_A, card_A, values_A)
+# factor_B = Factor(var_B, card_B, values_B)
+# factor_C = Factor(var_C, card_C, values_C)
+# factor_D = Factor(var_D, card_D, values_D)
+# factor_AC = Factor(var_AC, card_AC, values_AC)
+# factor_AD = Factor(var_AD, card_AD, values_AD)
+# factor_BD = Factor(var_BD, card_BD, values_BD)
 
-	rest_variables = []
-	start_index = 0
+# factor_list = [factor_A, factor_B, factor_C, factor_D, factor_AC, factor_AD, factor_BD]
 
-	for i in range(num_variables):
-		if i != x_id:
-			rest_variables.append(i)
+# print('Creating dataset...')
+# dataset = Gibbs_sampling(factor_list, np.array([1,1,2,1]), 20000)
+# print('Dataset created \n')
 
-	for y_id in rest_variables:
-		end_index = start_index + cardinality[y_id]
+# ----------------------------------------------------------------
+# num_variables = dataset.shape[1]
+# pairwise_factors = []
+# node_factors = []
+# factor_list = []
 
-		if y_id > x_id:
-			c_weights = pairwise_factors[x_id]
-			c_factor_values_mat = c_weights[start_index:end_index, :]
-			c_factor_values = np.reshape(c_factor_values_mat, (-1), order='F')
+# for x_id in range(num_variables):
+# 	tf.reset_default_graph()
+# 	new_dataset, Y, cardinality = transform_dataset(x_id, dataset)
 
-			# Pairwise factor
-			if np.linalg.norm(c_factor_values) > 1E-6:
-				adj_matrix[x_id, y_id] = 1
-				adj_matrix[y_id, x_id] = 1
-				new_Factor = Factor(np.array([x_id, y_id]), np.array([cardinality[x_id], cardinality[y_id]]), c_factor_values)
-				factor_list.append(new_Factor)
-		start_index = end_index
+# 	num_iter = 10000
+# 	batch_size = 5000
+# 	optimizer_name = tf.train.AdamOptimizer
+
+# 	name = 'Test' 
+# 	dim_classifier = np.array([new_dataset.shape[1], cardinality[x_id]])
+# 	save_dir='./trained_variables_fc_H1_1000'
+
+# 	F_C_Net = f_c_softmax_Group_LASSO(name, dim_classifier, cardinality, alpha=.01, save_dir=save_dir)
+# 	F_C_Net.train(optimizer_name, new_dataset, Y, num_iter=num_iter, batch_size=batch_size)
+
+# 	pairwise_factors.append(F_C_Net.final_weights)
+# 	node_factors.append(F_C_Net.final_bias)
+
+# # Create an empty adjacency matrix
+# adj_matrix = np.zeros((num_variables, num_variables), dtype=np.int8)
+
+# # Create the node and pairwise factor
+# for x_id in range(num_variables):
+
+# 	# Node factor
+# 	new_Factor = Factor(np.array([x_id]), np.array([cardinality[x_id]]), np.exp(node_factors[x_id]))
+# 	factor_list.append(new_Factor)
+
+# 	rest_variables = []
+# 	start_index = 0
+
+# 	for i in range(num_variables):
+# 		if i != x_id:
+# 			rest_variables.append(i)
+
+# 	for y_id in rest_variables:
+# 		end_index = start_index + cardinality[y_id]
+
+# 		if y_id > x_id:
+# 			c_weights = pairwise_factors[x_id]
+# 			c_factor_values_mat = c_weights[start_index:end_index, :]
+# 			c_factor_values = np.reshape(c_factor_values_mat, (-1), order='F')
+
+# 			# Pairwise factor
+# 			if np.linalg.norm(c_factor_values) > 1E-6:
+# 				adj_matrix[x_id, y_id] = 1
+# 				adj_matrix[y_id, x_id] = 1
+# 				new_Factor = Factor(np.array([x_id, y_id]), np.array([cardinality[x_id], cardinality[y_id]]), np.exp(c_factor_values))
+# 				factor_list.append(new_Factor)
+# 		start_index = end_index
 
 
-for factor in factor_list:
-	print(factor.variables)
-	print(factor.cardinality)
-	print(factor.values)
-	print('\n')
+# for factor in factor_list:
+# 	print(factor.variables)
+# 	print(factor.cardinality)
+# 	print(factor.values)
+# 	print('\n')
 
-print(adj_matrix)
+# print(adj_matrix)
+
+# jointDistribution = Factor(factor_list[0].variables, factor_list[0].cardinality, factor_list[0].values)
+
+# for i in range(len(factor_list)-1):
+# 	jointDistribution = FactorProduct(jointDistribution, factor_list[i+1])
+
+# Z = np.sum(jointDistribution.values)
+# print(Z)
+
+# var_A = np.array([0])
+# card_A = np.array([3])
+# values_A = np.array([10, 100, 20])
+
+# var_B = np.array([10])
+# card_B = np.array([2])
+# values_B = np.array([5, 2])
+
+# factor_A = Factor(var_A, card_A, values_A)
+# factor_B = Factor(var_B, card_B, values_B)
